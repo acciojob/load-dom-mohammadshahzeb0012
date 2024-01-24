52a3100325da3ed9472dfc0c87a96df38481a6c7
Mohammad Shahzeb <113281464+mohammadshahzeb0012@users.noreply.github.com>
2024-01-24 17:13:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded",()=>{
+  const h1 = document.createElement("h1");
+  h1.innerText = "DOM load success";
+  document.append(h1)
+})
